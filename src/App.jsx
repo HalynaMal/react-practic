8f330b6d1ce4a191bce7112462ed324964ab7486
HalynaMal <dguoit@ukr.net>
2024-03-09 19:17:40 +0200

    practic

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import userData from "./userData.json";
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json"
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+       <FriendList friends={friends} />
+    </>
+  );
+};
+
+export default App;
